d0ee8b263c764010ba44bb10d02c83a73ae07fbf
Camarco <camarcovincent@gmail.com>
2024-08-13 18:00:25 -0400

    added typedefs and resolvers, updated gitignore, refactored auth.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,56 @@
-const resolvers = {}
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, { _id }) => {
+            const user = await User.findById(_id);
+
+            if (!user) {
+                throw new Error('Cannot find a user with this id!');
+            }
+
+            return user;
+        }
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) throw new AuthenticationError;
+
+            const isCorrectPassword = user.isCorrectPassword(password);
+            if (!isCorrectPassword) throw new AuthenticationError;
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        addUser: async (parent, { username, email, password }) => {
+            try {
+                const user = await User.create({ username, email, password });
+                const token = signToken(user);
+
+                return { token, user };
+            } catch (e) {
+                console.error(e);
+            }
+        },
+        saveBook: async (parent, { userId, input }) => {
+            const user = await User.findById(userId);
+            if (user.savedBooks === null) user.savedBooks = [];
+            user.savedBooks.push(input);
+            user.save();
+
+            return user;
+        },
+        removeBook: async (parent, { userId, bookId }) => {
+            let user = await User.findById(userId);
+            user.savedBooks = user.savedBooks.filter(e => e.bookId !== bookId);
+            user.save();
+
+            return user;
+        }
+    }
+};
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,46 @@
-const typeDefs = ``
+const typeDefs = `
+    type Book {
+        authors: [String]
+        description: String!
+        bookId: ID!
+        image: String
+        link: String
+        title: String!
+    }
+    
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        password: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    
+    type Auth {
+        token: String
+        user: User
+    }
+    
+    type Query {
+        me(_id: ID!): User
+    }
+    
+    input BookInput {
+        authors: [String]
+        description: String!
+        title: String!
+        bookId: String!
+        image: String
+        link: String
+    }
+    
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(userId: ID!, input: BookInput!): User
+        removeBook(userId: ID!, bookId: ID!): User
+    }
+`;
 
 module.exports = typeDefs;
